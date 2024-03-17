6f12ae3077e00cab2217cf9a77f50b071b4b9faa
Braulio <braulio.sassi@gmail.com>
2024-03-17 20:02:47 -0300

    Capturando via twitter card

--- src/main/java/com/braulio/demoajax/service/SocialMetaTagService.java
+++ src/main/java/com/braulio/demoajax/service/SocialMetaTagService.java
@@ -15,9 +15,23 @@ public class SocialMetaTagService {
         try {
             Document doc = Jsoup.connect(url).userAgent("Mozzila").get();
             tag.setTitle(doc.head().select("meta[property=og:title]").attr("content"));
-            tag.setSite(doc.head().select("meta[property=og:title]").attr("content"));
-            tag.setImage(doc.head().select("meta[property=og:title]").attr("content"));
-            tag.setUrl(doc.head().select("meta[property=og:title]").attr("content"));
+            tag.setSite(doc.head().select("meta[property=og:site]").attr("content"));
+            tag.setImage(doc.head().select("meta[property=og:image]").attr("content"));
+            tag.setUrl(doc.head().select("meta[property=og:url]").attr("content"));
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+        return tag;
+    }
+
+    public SocialMetaTag getTwotterCardByUrl(String url) {
+        SocialMetaTag tag = new SocialMetaTag();
+        try {
+            Document doc = Jsoup.connect(url).userAgent("Mozzila").get();
+            tag.setTitle(doc.head().select("meta[name=twitter:title]").attr("content"));
+            tag.setSite(doc.head().select("meta[name=twitter:site]").attr("content"));
+            tag.setImage(doc.head().select("meta[name=twitter:image]").attr("content"));
+            tag.setUrl(doc.head().select("meta[name=twitter:url]").attr("content"));
         } catch(IOException e) {
             e.printStackTrace();
         }

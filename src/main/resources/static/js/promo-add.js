57c060953b495a1f9628e7707cc08671752f94ac
Braulio <braulio.sassi@gmail.com>
2024-03-19 00:02:26 -0300

    Implementando função ajax

---
+++ src/main/resources/static/js/promo-add.js
@@ -0,0 +1,15 @@
+//funcao para capturar metatags
+$("#linkPromocao").on('change', function() {
+    var url = $(this).val();
+
+    if(url.length > 7) {
+        $.ajax({
+            method:"POST",
+            url: "/meta/info?url=" +url,
+            cache: false,
+            success: function( data ) {
+                console.log(data);
+            }
+        })
+    }
+});
